8391bbfc5b880c318fe96cdc423851a339813b36
Michael Tornaritis <michael.tornaritis@example.com>
2026-01-30 17:18:12 -0600

    chore: Add pages directory files for Next.js build manifest

---
+++ pages/__app-router-placeholder.tsx
@@ -0,0 +1,9 @@
+import type { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return { notFound: true };
+};
+
+export default function AppRouterPlaceholderPage() {
+  return null;
+}
